ce65123c8cc33af34b3a1677a699ed4d7e36b023
Pokhvalenskii <pokhvalenskii@gmail.com>
2021-06-26 15:38:21 +0300

    style: добавил стили компонента profile и profile-form

---
+++ src/components/Movies.js
@@ -0,0 +1,9 @@
+function Movies (props) {
+  return(
+    <section className='movies'>
+      <form className='search-form'></form>
+    </section>
+  );
+}
+
+export default Movies;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,19 +5,28 @@ function Profile (props) {
   return(
     <section className='profile'>
       <div className='profile__header'>
-        <img className='logo' src={logo} alt='Логотип'/>
-        <nav className='profile__nav'>
-          <p className='profile__text profile__text_type_fat'>Фильмы</p>
-          <p className='profile__text'>Сохранённые фильмы</p>
-          <p className='profile__text'>Аккаунт</p>
-          <div className='icon-block'>
-            <img className='icon-block__img' src={icon} alt='иконка профиля'/>
-          </div>
-        </nav>        
+        <div className='profile__wrapper'>
+          <img className='logo' src={logo} alt='Логотип'/>
+          <nav className='profile__nav'>
+            <p className='profile__text profile__text_type_fat'>Фильмы</p>
+            <p className='profile__text'>Сохранённые фильмы</p>
+            <p className='profile__text profile__text_type_fat'>Аккаунт</p>
+            <div className='icon-block'>
+              <img className='icon-block__img' src={icon} alt='иконка профиля'/>
+            </div>
+          </nav>
+        </div>
         <form className='profile-form'>
           <h2 className='profile-form__title'>Привет, Виталий!</h2>
-          <input className='profile-form__input' type='text'/>
-          <input className='profile-form__input' type='email'/>
+          <div className='profile-form__input-wrapper'>
+            <p className='profile-form__input-label'>Имя</p>
+            <input className='profile-form__input' type='text' dir='rtl' maxLength='30' minLength='1'
+            placeholder='Имя'/>
+          </div>
+          <div className='profile-form__input-wrapper'>
+            <p className='profile-form__input-label'>E-mail</p>
+            <input className='profile-form__input' type='email' dir='rtl' maxLength='30' minLength='1' placeholder='E-mail'/>
+          </div>
           <button className='profile-form__btn profile-form__btn_type_edit'>Редактировать</button>
           <button className='profile-form__btn profile-form__btn_type_exit'>Выйти из аккаунта</button>
         </form>

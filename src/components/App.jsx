f9cb0ea4767574759a41e2f782d5d8a790da0903
Pokhvalenskii <pokhvalenskii@gmail.com>
2021-06-28 05:03:59 +0300

    style: поменял стили Section-title

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -35,7 +35,7 @@ function App() {
         <Route exact path='/'>
           <Header />
           <Main />
-          {/* <Project /> */}
+          <Project />
           {/* <Technologies /> */}
           {/* <Student /> */}
           {/* <Footer /> */}

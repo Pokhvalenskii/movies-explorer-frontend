815e9229b032308c0979059f4d823de898b5d780
Pokhvalenskii <pokhvalenskii@gmail.com>
2021-06-28 22:53:24 +0300

    ref: сменил тему на черную

--- src/components/HeaderProfile.jsx
+++ src/components/HeaderProfile.jsx
@@ -1,5 +1,6 @@
 import logo from '../images/logo.svg';
 import icon from '../images/icon-user.svg';
+import profilelogo from '../images/profile-btn.svg'
 
 import Menu from './Menu';
 
@@ -15,10 +16,9 @@ function HeaderProfile (props) {
       <nav className='header-profile__nav'>
         <a href='/movies' className='header-profile__text header-profile__text_type_fat'>Фильмы</a>
         <a href='/saved-movies' className='header-profile__text'>Сохранённые фильмы</a>
-        <a href='/profile' className='header-profile__text header-profile__text_type_fat'>Аккаунт</a>
-        <div className='icon-block'>
-          <img className='icon-block__img' src={icon} alt='иконка профиля'/>
-        </div>
+        <a href='/profile' className='header-profile__text header-profile__text_type_fat'>
+          <img src={profilelogo} alt="Логотип профиля" />
+        </a>
       </nav>
       <div className={`menu-btn ${isActive}`} onClick={props.isActive}>
         <span className='menu-btn__line'></span>

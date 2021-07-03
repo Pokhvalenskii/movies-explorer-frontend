dff5e6e39790deeafec9ac1b972500fe06aba55c
Pokhvalenskii <pokhvalenskii@gmail.com>
2021-07-03 17:58:42 +0300

    feat: дописал MainApi

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -38,6 +38,79 @@ class MainApi {
       .then(this._checkRes);
   }
 
+  _getMe (data) {
+    return fetch (`${this._urlApi}/users/me`, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+        'Authorization': `Bearer ${data.jwt}`
+      }
+    })
+      .then(this._checkRes);
+  }
+
+  _editUser (data) {
+    return fetch (`${this._urlApi}/users/me`, {
+      method: 'PATCH',
+      headers: {
+        'Content-type': 'application/json',
+        'Authorization': `Bearer ${data.jwt}`,
+      },
+      body: JSON.stringify({
+        'email': data.email,
+        'name': data.name,
+      })
+    })
+      .then(this._checkRes);
+  }
+
+  _getMovies (data) {
+    return fetch (`${this._urlApi}/movies`, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+        'Authorization': `Bearer ${data.jwt}`,
+      }
+    })
+      .then(this._checkRes);
+  }
+
+  _addMovie (data) {
+    return fetch (`${this._urlApi}/movies`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        'Authorization': `Bearer ${data.jwt}`,
+      },
+      body: JSON.stringify({
+        'country': data.country,
+        'director': data.director,
+        'duration': data.duration,
+        'year': data.year,
+        'description': data.description,
+        'image': data.image,
+        'trailer': data.trailer,
+        'nameRU': data.nameRU,
+        'nameEN': data.nameEN,
+        'thumbnail': data.thumbnail,
+        'movieId': data.movieId,
+      })
+    })
+      .then(this._checkRes);
+  }
+
+  _deleteMovie (data) {
+    return fetch (`${this._urlApi}/movies/movieId`, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json',
+        'Authorization': `Bearer ${data.jwt}`,
+      }
+    })
+      .then(this._checkRes);
+  }
+
+
 }
 
 const mainApi = new MainApi({

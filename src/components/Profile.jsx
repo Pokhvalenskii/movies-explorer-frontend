69c3fcdf2fc57bcf0d70075d2ab25258b8e04955
Pokhvalenskii <pokhvalenskii@gmail.com>
2021-07-11 07:14:32 +0300

    fix: валидация формы редактирования  профиля

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,6 @@
 import HeaderProfile from './HeaderProfile';
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
 
 function Profile (props) {
 
@@ -8,6 +8,39 @@ function Profile (props) {
   const [userName, setUserName] = useState('');
   const [userEmail, setUserEmail] = useState('');
 
+
+  const [validate, setValidate] = useState(false)
+
+  const [emailError, setEmailError] = useState('Нужно заполнить E-mail')
+  const [nameError, setNameError] = useState('Нужно заполнить пароль')
+
+  const [clearEmail, setClearEmail] = useState(false);
+  const [clearName, setClearName] = useState(false);
+
+  const status = validate ? '' : 'profile-form__btn_type_disabled';
+  const patternName = /^[A-Za-zА-яёЁ -]+$/;
+  const patternEmail = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
+
+  useEffect(() => {
+    if(emailError || nameError) {
+      setValidate(false);
+    } else {
+      setValidate(true)
+    }
+  }, [emailError, nameError]);
+
+  const checkStateErrors = (e) => {
+    switch(e.target.name) {
+      case 'email':
+        setClearEmail(true)
+        break
+      case 'name':
+        setClearName(true)
+        break
+      default :
+        break
+    }
+  }
 
   function submit (e) {
     e.preventDefault();
@@ -20,10 +53,26 @@ function Profile (props) {
 
   function handleChangeUserName (e) {
     setUserName(e.target.value)
+    if(!e.target.value.match(patternName)) {
+      setNameError('Только латиницу, кириллицу, пробел или дефис');
+      if(!e.target.value) {
+        setNameError('Нужно заполнить имя')
+      }
+    } else {
+      setNameError('')
+    }
   }
 
   function handleChangeUserEmail (e) {
     setUserEmail(e.target.value)
+    if(!e.target.value.match(patternEmail)) {
+      setEmailError('Неправильный E-mail');
+      if(!e.target.value) {
+        setEmailError('Нужно заполнить E-mail')
+      }
+    } else {
+      setEmailError('')
+    }
   }
 
   function logout () {
@@ -39,30 +88,39 @@ function Profile (props) {
         <div className='profile-form__input-wrapper'>
           <p className='profile-form__input-label'>Имя</p>
           <input
+            name='name'
+            onBlur={e => checkStateErrors(e)}
+
             className='profile-form__input'
             type='text'
-            // dir='rtl'
             maxLength='30'
             minLength='1'
             placeholder={currentUser.name}
             value={userName}
             onChange={handleChangeUserName}
-          />
+          />          
         </div>
+        {(nameError && clearName) && <span className='form__error'>{nameError}</span>}
         <div className='profile-form__input-wrapper'>
           <p className='profile-form__input-label'>E-mail</p>
           <input
+            name='email'
+            onBlur={e => checkStateErrors(e)}
+
             className='profile-form__input'
             type='email'
-            // dir='rtl'
             maxLength='30'
             minLength='1'
             placeholder={currentUser.email}
             value={userEmail}
             onChange={handleChangeUserEmail}
-          />
+          />          
         </div>
-        <button className='profile-form__btn profile-form__btn_type_edit'>Редактировать</button>
+        {(emailError && clearEmail) && <span className='form__error'>{emailError}</span>}
+        <button 
+        className={`profile-form__btn profile-form__btn_type_edit ${status}`}
+        disabled={!validate}
+        >Редактировать</button>
         <button className='profile-form__btn profile-form__btn_type_exit' onClick={logout}>Выйти из аккаунта</button>
       </form>
     </section>
